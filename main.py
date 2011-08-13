8cf86504ba47e2490ad2a7afd198d6d6c4ad5fdd
Diptanu Choudhury <diptanuc@gmail.com>
2011-08-13 12:08:25 +0530

    Adding a base template

--- main.py
+++ main.py
@@ -1,26 +1,7 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
-from apiclient.discovery import build
-import httplib2
-from oauth2client.appengine import OAuth2Decorator
-import settings
+from main_handler import MainHandler
 
-decorator = OAuth2Decorator(client_id=settings.CLIENT_ID,
-                            client_secret=settings.CLIENT_SECRET,
-                            scope=settings.SCOPE,
-                            user_agent='mytasks')
-
-
-class MainHandler(webapp.RequestHandler):
-
-   @decorator.oauth_required
-   def get(self):
-     service = build('tasks', 'v1', http=decorator.http())
-     tasks = service.tasks().list(tasklist='@default').execute()
-     self.response.out.write('<html><body><ul>')
-     for task in tasks['items']:
-       self.response.out.write('<li>%s</li>' % task['title'])
-     self.response.out.write('</ul></body><html>')
 
 application = webapp.WSGIApplication([('/', MainHandler)], debug=True)
 

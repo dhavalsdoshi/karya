e4bf0e414d4f84fb8e48f4f2a756cdfb3d0f2b21
Divyanshu Mohan <divyanm@thoughtworks.com>
2011-09-09 14:57:11 +0530

    div - basic styling for a single task list

--- static/js/script.js
+++ static/js/script.js
@@ -1,33 +1,33 @@
 var test;
 $(function() {
 	$( "#sortable" ).sortable({
-	        revert: true
-	    });
-	    $( "ul, li" ).disableSelection();
+    revert: true
+  });
+  $( "ul, li" ).disableSelection();
 	
 	function task(title, isDone, ownerViewModel) {
-        this.title = ko.observable(title);
-        this.isDone = ko.observable(isDone);
-        this.remove = function() { ownerViewModel.tasks.remove(this) }
-    }
+    this.title = ko.observable(title);
+    this.isDone = ko.observable(isDone);
+    this.remove = function() { ownerViewModel.tasks.remove(this) };
+  }
 
-    function taskListViewModel() {
-        this.tasks = ko.observableArray([]);
-        this.newTaskText = ko.observable();
-        this.addTask = function() {
-            this.tasks.push(new task(this.newTaskText(), false, this));
-            this.newTaskText("");    
-        }
-        this.incompleteTasks = ko.dependentObservable(function() {
-            return ko.utils.arrayFilter(this.tasks(), function(task) { return !task.isDone(); });
-        }, this);
-        self = this;
-        $.getJSON('/tasks', function(data){
-            $.map(data, function(item){
-                self.tasks.push(new task(item.name, item.isDone, self));
-            })
-        });
+  function taskListViewModel() {
+    this.tasks = ko.observableArray([]);
+    this.newTaskText = ko.observable();
+    this.addTask = function() {
+      this.tasks.push(new task(this.newTaskText(), false, this));
+      this.newTaskText("");
     }
+    this.incompleteTasks = ko.dependentObservable(function() {
+      return ko.utils.arrayFilter(this.tasks(), function(task) { return !task.isDone(); });
+    }, this);
+    self = this;
+    $.getJSON('/tasks', function(data){
+      $.map(data, function(item){
+          self.tasks.push(new task(item.name, item.isDone, self));
+      })
+    });
+  }
 
-    ko.applyBindings(new taskListViewModel());
+  ko.applyBindings(new taskListViewModel());
 });
